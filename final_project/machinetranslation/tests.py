de4233331fc09b8477531c3743b18decfe6994dc
gonzalo9991r <go.rojasf@duocuc.cl>
2023-05-12 22:41:33 -0400

    ADD: new changes to tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,11 +3,17 @@ from translator import Translator
 
 
 class TestTranslator(unittest.TestCase):
-    def test(self):
+    def test_english_to_french_equal(self):
         self.assertEqual(Translator().english_to_french('Hello'), 'Bonjour')
-        self.assertEqual(Translator().english_to_french('Hello'), 'Bonjour')
+
+    def test_english_to_french_not_equal(self):
+        self.assertNotEqual(Translator().english_to_french('Hello'), 'bonjour')
+
+    def test_french_to_english_equal(self):
         self.assertEqual(Translator().french_to_english('Bonjour'), 'Hello')
-        self.assertEqual(Translator().french_to_english('Bonjour'), 'Hello')
+
+    def test_french_to_english_not_equal(self):
+        self.assertNotEqual(Translator().french_to_english('Bonjour'), 'hello')
 
 
 unittest.main()
